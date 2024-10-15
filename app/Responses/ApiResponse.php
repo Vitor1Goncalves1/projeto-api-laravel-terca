a02ace5adc8157777a5cece4320d2da00077b1cc
Vitor1Goncalves1 <darknoplayxd@gmail.com>
2024-10-15 19:25:48 -0300

    initial config

---
+++ app/Responses/ApiResponse.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Responses;
+
+class ApiResponse
+{
+    public static function success(?string $message = null, mixed $data = null)
+    {
+        return response()->json([
+            "success"=> "success",
+            "message" => $message,
+            "data" => $data
+        ], 200);
+
+    }
+
+    public static function error(string $message, mixed $data = null)
+    {
+        return response()->json([
+            "status" => "fail",
+            "message" => $message,
+            "data" => $data
+        ], 400);
+    }
+}
